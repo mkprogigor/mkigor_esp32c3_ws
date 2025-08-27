f41a2e7f09c244be0d4e59e85539d07d8802f6a5
Igor Mkprog <mkprogigor@gmail.com>
2025-08-27 10:14:28 +0200

    commit28

---
+++ include/mydef.h
@@ -0,0 +1,10 @@
+// local definitions
+#ifndef MYDEF_H
+#define MYDEF_H
+
+#define WIFI_SSID "POCOLUDA"
+#define WIFI_PASS "123456789"
+#define MYTS_WR_APIKEY "JVJ72ILLDFV0N0X4";
+#define MYTS_RD_APIKEY "I0KTR29NKBSLKH7B";
+
+#endif
